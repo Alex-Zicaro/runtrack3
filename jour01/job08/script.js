d778a95ba4f0d99ffef172dc0f0a9c4ac1683c3c
Alex-Zicaro <alex.zicaro@laplateforme.io>
2022-03-13 23:21:11 +0100

    job07

--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -2,7 +2,3 @@
 deux variables. Si ces deux variables sont des nombres premiers, alors la
 fonction retourne leur somme. Sinon, la fonction retourne false. */
 
-function sommeNombresPremiers(int , intTwo){
-
-    if(int %  == 0 && int)
-}
